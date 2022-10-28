27b230bbf1857e07256cccb9f9aca35a915ce174
visawjeet mallik <vishmallik@gmail.com>
2022-10-29 00:12:04 +0530

    initial commit

---
+++ block-BRaabm/movie-list/src/components/Movies.jsx
@@ -0,0 +1,45 @@
+import moviesList from "../data.json";
+// import Movie from "./Movie";
+import Modal from "./Modal";
+import React from "react";
+
+class Movies extends React.Component {
+  constructor(props) {
+    super(props);
+    this.state = {
+      activeIndex: null,
+    };
+  }
+  clickHandler = (i) => {
+    this.setState({
+      activeIndex: i,
+    });
+  };
+  render() {
+    return (
+      <ul className="flex wrap container movies">
+        {moviesList.map((movie, index) => {
+          return (
+            <li>
+              <div className="img-container">
+                <img src={movie.Poster} alt={movie.Title} />
+                <div className="flex details">
+                  <button onClick={() => this.clickHandler(index)}>
+                    More Info
+                  </button>
+                </div>
+              </div>
+              <h2>{movie.Title}</h2>
+              <p>Released: {movie.Released}</p>
+              {this.state.activeIndex === index ? (
+                <Modal data={movie} setState={this.clickHandler} />
+              ) : null}
+            </li>
+          );
+        })}
+      </ul>
+    );
+  }
+}
+
+export default Movies;
